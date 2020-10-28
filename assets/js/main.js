afe6b26cf07f638cae33270afb1df69cab75d254
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-10-28 20:05:47 +0100

    Einkommensteuer-Berechnung

---
+++ assets/js/main.js
@@ -0,0 +1,124 @@
+
+function myFunction() {
+    document.getElementById("id-einkommen2").style.display="unset"
+ }
+
+function Berechnen() {
+
+    var jahr = document.getElementById("liste").value;
+
+    jahr = Number(jahr);
+    var einkommen1 = document.getElementById("einkommen1").value;
+    einkommen1 = Number(einkommen1);
+    var einkommen2 = document.getElementById("einkommen2").value;
+    einkommen2 = Number(einkommen2);
+    var radio_1 = document.getElementById("einperson");
+    var radio_2 = document.getElementById("zweiperson");
+    var result = document.getElementById("result");
+    var tax;
+   
+   
+    
+    if(!radio_2.checked) {
+        var zvE = einkommen1;
+        
+    }else {
+        var zvE = (einkommen1 + einkommen2)/2;
+        
+    }
+
+    
+    if(jahr === 2020) {
+           if(zvE <= 9408) {
+               Est =0;
+               result.innerHTML = Est + ' Euro';
+
+           } else if(zvE >= 9409 && zvE<= 14532) {
+               y = (zvE - 9408)/10000;
+               Est = (972.87*y +1400)* y;
+               Est = Est.toFixed(2);
+               result.innerHTML = Est + ' Euro';
+           } else if(zvE>= 14533 && zvE<= 57051) {
+               z =(zvE - 14532)/10000;
+               Est = (212.02 *z  + 2397) * z + 972.79;
+               Est = Est.toFixed(2);
+               result.innerHTML = Est + ' Euro';
+           } else if(zvE>= 57052 && zvE<= 270500 ) {
+            Est = 0.42 * zvE - 8963.74;
+            Est = tax.toFixed(2);
+                result.innerHTML = Est + ' Euro';
+           } else {
+            Est = 0.45 * zvE - 17078.74;
+            Est = Est.toFixed(2);
+               result.innerHTML = Est + ' Euro';
+           }
+
+
+       
+       
+   } 
+   
+   else if(jahr === 2019) {
+    console.log("we are in 2019");
+
+        if(zvE <= 9168) {
+            Est =0;
+            result.innerHTML = Est + ' Euro';
+
+        } else if(zvE >= 9169 && zvE<= 14254) {
+            y = (zvE - 9168)/10000;
+            Est = (980.14*y +1400)* y;
+            Est = Est.toFixed(2);
+            result.innerHTML = Est + ' Euro';
+        } else if(zvE>= 14255 && zvE<= 55960) {
+            tax =(zvE - 14532)/10000;
+            tax = (212.02 *tax  + 2397) * tax + 972.79;
+            tax = tax.toFixed(2);
+            result.innerHTML = tax + ' Euro';
+        } else if(zvE>= 57052 && zvE<= 270500 ) {
+             tax = 0.42 * zvE - 8963.74;
+             tax = tax.toFixed(2);
+             result.innerHTML = tax + ' Euro';
+        } else {
+            tax = 0.45 * zvE - 17078.74;
+            tax = tax.toFixed(2);
+            result.innerHTML = tax + ' Euro';
+        }  
+   }
+   
+   else {
+    
+
+    if(zvE <= 9000) {
+        tax =0;
+        result.innerHTML = tax + ' Euro';
+
+    } else if(zvE >= 9001 && zvE<= 13996) {
+        tax = (zvE - 9000)/10000;
+        tax = (997.80*tax +1400)* tax;
+        tax = tax.toFixed(2);
+        result.innerHTML = tax + ' Euro';
+    } else if(zvE>= 13997 && zvE<= 54949) {
+        tax =(zvE - 13996)/10000;
+        tax = (220.13 *tax  + 2397) * tax + 948.49;
+        tax = tax.toFixed(2);
+        result.innerHTML = tax + ' Euro';
+    } else if(zvE>= 54950 && zvE<= 260532 ) {
+         tax = 0.42 * zvE - 8621.75;
+         tax = tax.toFixed(2);
+         result.innerHTML = tax + ' Euro';
+    } else {
+        tax = 0.45 * zvE - 16437.70;
+        tax = tax.toFixed(2);
+        result.innerHTML = tax + ' Euro';
+    }
+       
+   }
+    
+}
+
+
+
+    
+    
+ 
