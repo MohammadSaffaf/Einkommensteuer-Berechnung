5edab82eca752298ec78a5f73afc8b66c86b64fd
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-10-29 15:38:36 +0100

    Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+//Ausgabe der Uhrzeit:
+function uhrzeit() {
+    var jetzt = new Date(),
+        h = jetzt.getHours(),
+        m = jetzt.getMinutes(),
+        s = jetzt.getSeconds();
+    m = fuehrendeNull(m);
+    s = fuehrendeNull(s);
+    document.getElementById('uhr').innerHTML = h + ':' + m + ':' + s;
+    setTimeout(uhrzeit, 500);
+  }
+  
+  function fuehrendeNull(zahl) {
+    zahl = (zahl < 10 ? '0' : '' )+ zahl;  
+    return zahl;
+  }
+
+
 
 function myFunction() {
     document.getElementById("id-einkommen2").style.display="unset"
